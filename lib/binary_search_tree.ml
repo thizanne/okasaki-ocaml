4303cd51f5f02bf6f4144264e441cb786a509c6f
Thibault Suzanne <thi.suzanne@gmail.com>
2021-06-15 20:44:46 +0200

    Factor out signatures in binary_search_tree_intf

--- lib/binary_search_tree.ml
+++ lib/binary_search_tree.ml
@@ -1,54 +1,6 @@
-module type Set = sig
-  type elem
-  type t
-
-  val empty : t
-  val insert : elem -> t -> t
-  val member : elem -> t -> bool
-
-  (* Bonus, for easing experiments *)
-  val to_string : t -> string
-end
-
-module type Solutions = sig
-
-  include Set
-
-  module Exercise_2_2 : sig
-
-    module With_option : sig
-      val member : elem -> t -> bool
-    end
-
-    module With_duplication : sig
-      val member : elem -> t -> bool
-    end
-
-    module Using_root : sig
-      val member : elem -> t -> bool
-    end
-
-    module With_gadt : sig
-      val member : elem -> t -> bool
-    end
-
-  end
-
-  module Exercise_2_3 : sig
-    val insert : elem -> t -> t
-  end
-
-  module Exercise_2_4 : sig
-    val insert : elem -> t -> t
-  end
-
-  module Exercise_2_5 : sig
-    val complete : depth:int -> elem -> t
-    val balanced : size:int -> elem -> t
-  end
-end
-
-module Unbalanced_set (Element : Sig.Ordered) : Solutions with type elem = Element.t =
+module Unbalanced_set (Element : Sig.Ordered) :
+  Binary_search_tree_intf.Solutions
+  with type elem = Element.t =
 struct
 
   type elem = Element.t
--- lib/binary_search_tree.mli
+++ lib/binary_search_tree.mli
@@ -10,44 +10,9 @@ module type Set = sig
   val to_string : t -> string
 end
 
-module type Solutions = sig
-  include Set
+module Unbalanced_set (Element : Sig.Ordered) :
+  Binary_search_tree_intf.Solutions
+  with type elem = Element.t
 
-  module Exercise_2_2 : sig
-
-    module With_option : sig
-      val member : elem -> t -> bool
-    end
-
-    module With_duplication : sig
-      val member : elem -> t -> bool
-    end
-
-    module Using_root : sig
-      val member : elem -> t -> bool
-    end
-
-    module With_gadt : sig
-      val member : elem -> t -> bool
-    end
-
-  end
-
-  module Exercise_2_3 : sig
-    val insert : elem -> t -> t
-  end
-
-  module Exercise_2_4 : sig
-    val insert : elem -> t -> t
-  end
-
-  module Exercise_2_5 : sig
-    val complete : depth:int -> elem -> t
-    val balanced : size:int -> elem -> t
-  end
-end
-
-module Unbalanced_set (Element : Sig.Ordered) : Solutions with type elem = Element.t
-
-module I : Solutions with type elem = int
-module S : Solutions with type elem = string
+module I : Binary_search_tree_intf.Solutions with type elem = int
+module S : Binary_search_tree_intf.Solutions with type elem = string
