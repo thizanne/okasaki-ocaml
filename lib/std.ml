15df517614527ed8fec841929a4dbfeeb58d0ef1
Thibault Suzanne <thi.suzanne@gmail.com>
2021-06-15 23:00:57 +0200

    Add compare to Ordered

--- lib/std.ml
+++ lib/std.ml
@@ -8,12 +8,15 @@ module type Ordered = sig
   val leq : t -> t -> bool
 
   (* Not included in Okasaki, easier for experiments *)
+  val compare : t -> t -> int
   val to_string : t -> string
+
 end
 
 (* For easier testing *)
 module Int_ordered : Ordered with type t = int = struct
   type t = int
+  let compare = compare
   let lt = ( < )
   let eq = ( = )
   let leq = (<=)
@@ -22,6 +25,7 @@ end
 
 module String_ordered : Ordered with type t = string = struct
   type t = string
+  let compare = compare
   let lt = ( < )
   let eq = ( = )
   let leq = (<=)
